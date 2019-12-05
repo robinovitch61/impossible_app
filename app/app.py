5a531d13a74978eca149281976983fa354a869e6
robinovitch61 <leorobinovitch@gmail.com>
2019-12-05 12:18:02 -0500

    refactor html, add nav

--- app/app.py
+++ app/app.py
@@ -26,10 +26,8 @@ DB_NAME = 'microbes'
 bootstrap(DB_NAME)
 db = MySQLConn(app, DB_NAME)
 
-# on user visit to landing page
 @app.route('/', methods=("GET", "POST"))
-def index():
-
+def strain():
     # add strain
     strain_form = StrainAddForm(request.form)    
     if strain_form.validate_on_submit():
@@ -49,6 +47,17 @@ def index():
         db.remove_from_table('strain', 'strain_id = {}'.format(strain_id))
         return redirect('/')
 
+    df_strain = db.query_data("SELECT * FROM {db}.strain")
+
+    return render_template(
+        'strain.html',
+        strain=df_strain.to_html(classes='data', header="true", index=False),
+        strain_form=strain_form,
+        strain_del_form=strain_del_form,
+    )
+
+@app.route('/plasmid', methods=("GET", "POST"))
+def plasmid():
     # add plasmid
     plasmid_form = PlasmidAddForm(request.form)    
     if plasmid_form.validate_on_submit():
@@ -60,14 +69,14 @@ def index():
             'notes': plasmid_form.notes.data,
         }
         db.insert_into_table('plasmid', data)
-        return redirect('/')
+        return redirect('/plasmid')
 
     # delete plasmid
     plasmid_del_form = PlasmidDeleteForm(request.form)
     if plasmid_del_form.validate_on_submit():
         plasmid_id = plasmid_del_form.plasmid_id.data
         db.remove_from_table('plasmid', 'plasmid_id = {}'.format(plasmid_id))
-        return redirect('/')
+        return redirect('/plasmid')
 
     # upload files for plasmid
     plasmid_file_form = UploadForm(request.form)
@@ -81,8 +90,20 @@ def index():
             all_files.append(full_path)
             _file.save(full_path)
         app.logger.debug(all_files)
-        return redirect('/')
+        return redirect('/plasmid')
     
+    df_plasmid = db.query_data("SELECT * FROM {db}.plasmid")
+
+    return render_template(
+        'plasmid.html',
+        plasmid=df_plasmid.to_html(classes='data', header="true", index=False),
+        plasmid_form=plasmid_form,
+        plasmid_del_form=plasmid_del_form,
+        plasmid_file_form=plasmid_file_form,
+    )
+
+@app.route('/gene', methods=("GET", "POST"))
+def gene():
     # add gene
     gene_form = GeneAddForm(request.form)
     if gene_form.validate_on_submit():
@@ -94,31 +115,37 @@ def index():
             'notes': gene_form.notes.data,
         }
         db.insert_into_table('gene', data)
-        return redirect('/')
+        return redirect('/gene')
 
     # delete gene
     gene_del_form = GeneDeleteForm(request.form)
     if gene_del_form.validate_on_submit():
         gene_id = gene_del_form.gene_id.data
         db.remove_from_table('gene', 'gene_id = {}'.format(gene_id))
-        return redirect('/')
+        return redirect('/gene')
 
-    df_strain = db.query_data("SELECT * FROM {db}.strain")
-    df_plasmid = db.query_data("SELECT * FROM {db}.plasmid")
+    # upload files for gene
+    gene_file_form = UploadForm(request.form)
+    if gene_file_form.validate_on_submit():
+        file_names = request.files.getlist(gene_file_form.files.name)
+        all_files = []
+        app.logger.debug(file_names)
+        for _file in file_names:
+            file_name = secure_filename(_file.filename)
+            full_path = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+            all_files.append(full_path)
+            _file.save(full_path)
+        app.logger.debug(all_files)
+        return redirect('/gene')
+
     df_gene = db.query_data("SELECT * FROM {db}.gene")
-    
+
     return render_template(
-        'index.html',
-        strain=df_strain.to_html(classes='data', header="true", index=False),
-        plasmid=df_plasmid.to_html(classes='data', header="true", index=False),
+        'gene.html',
         gene=df_gene.to_html(classes='data', header="true", index=False),
-        strain_form=strain_form,
-        strain_del_form=strain_del_form,
-        plasmid_form=plasmid_form,
-        plasmid_del_form=plasmid_del_form,
-        plasmid_file_form=plasmid_file_form,
         gene_form=gene_form,
         gene_del_form=gene_del_form,
+        gene_file_form=gene_file_form,
     )
 
 if __name__ == '__main__':
